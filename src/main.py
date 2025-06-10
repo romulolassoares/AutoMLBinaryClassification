5a9e0d84d9b91d57cdc4c690e3fbf674b1572f33
romulolass <romulolassoares@gmail.com>
2025-06-10 14:37:38 -0300

    Update create directories function

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 from modules.automl import Automl
 import pandas as pd
 import yaml
+from utils.create_folders import create_dirs
 
 config_path = "config.yaml"
 with open(config_path, "r") as file:
     config = yaml.safe_load(file)
 
+create_dirs(config)
+
 file = config["tests"]["dataset_file"]
 
 df = pd.read_csv(file)
 
-automl = Automl(df, file)
+automl = Automl(df, file, config=config)
 top_models = automl.find_best_models(2)
 tuned_model = automl.tuning_model(top_models[0])
 automl.eval_model(tuned_model)
--- src/utils/create_folders.py
+++ src/utils/create_folders.py
@@ -1,17 +1,9 @@
 import os
-import yaml
 
-
-def create_dirs_from_yaml(config_path: str) -> None:
-    with open(config_path, "r") as file:
-        config = yaml.safe_load(file)
-
+def create_dirs(config: dict) -> None:
     base_path = config["paths"].get("base", "")
     for key, sub_path in config["paths"].items():
         if key == "base":
             continue
         full_path = os.path.join(base_path, sub_path)
         os.makedirs(full_path, exist_ok=True)
-
-
-create_dirs_from_yaml("config.yaml")
