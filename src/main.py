e8db5c9402641fd2acdaa26109ad57011aa44676
Rômulo Soares <romulolassoares@gmail.com>
2025-12-28 17:30:49 -0300

    Update model tuning and evaluation process in Automl class to handle multiple models, enhancing performance assessment and output clarity.

--- src/main.py
+++ src/main.py
@@ -14,8 +14,26 @@ file = config["tests"]["dataset_file"]
 df = pd.read_csv(file)
 
 automl = Automl(df, file, config=config)
-top_models = automl.find_best_models(2)
-tuned_model = automl.tuning_model(top_models[0])
-automl.eval_model(tuned_model)
-automl.save_model(tuned_model)
+top_models = automl.find_best_models(4)
 
+tuned_models = []
+for model in top_models:
+    tuned_model = automl.tuning_model(model)
+    tuned_models.append(tuned_model)
+
+model_evaluations = []
+for model in tuned_models:
+    model_evaluation = automl.eval_model(model)
+    model_evaluations.append(model_evaluation)
+    
+#automl.save_model(model)
+
+for evaluation in model_evaluations:
+    print(f"{evaluation["model_name"]}: {evaluation["accuracy"]}")
+
+#forma de identificar overfiting
+# diferença da accuracy entr test e train
+# se o percentual for maior que uma porcentagem 10%
+# possivel caso
+
+#tem outra forma melhor????
